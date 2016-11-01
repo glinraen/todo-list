ff43cd11d9f6f175280a6d7d5b88222a683132f3
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-11-01 11:49:00 -0700

    adds setup, routes, config

---
+++ server.js
@@ -0,0 +1,27 @@
+    // set up 
+    var express  = require('express');
+    var app      = express();                        // create app w/ express
+    var mongoose = require('mongoose');              // mongoose for mongodb
+    var morgan   = require('morgan');                // log requests to the console 
+    var bodyParser = require('body-parser');         // pull information from HTML POST 
+    var methodOverride = require('method-override'); // simulate DELETE and PUT 
+    var database = require('./config/database');
+    var port     = process.env.PORT || 8888;         // set the port
+
+
+    // configuration 
+    mongoose.connect(database.url);     // connect to mongoDB 
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+    // routes 
+    require('./app/routes.js')(app);
+
+    // listen (start app with node server.js)
+    app.listen(port);
+    console.log("App listening on port : " + port);
