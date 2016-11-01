5cd04fb7dc8306305de5bd70caf6273c9a088e37
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-11-01 13:21:20 -0700

    adds model and adds todo list html and css

---
+++ app/routes.js
@@ -0,0 +1,58 @@
+
+// load the todo model
+var Todo = require('./models/todo');
+
+// expose the routes to app with module.exports
+module.exports = function(app) {
+ 
+    // get all todos
+    app.get('/api/todos', function(req, res) {
+
+    // use mongoose to get all todos in the database
+        Todo.find(function(err, todos) {
+
+            // if there is an error retrieving, send the error. 
+            if (err)
+                res.send(err);
+
+            res.json(todos); // return all todos in JSON format
+        });
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/todos', function(req, res) {
+
+        // create a todo, information comes from AJAX request from Angular
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err);
+                res.json(todos);
+            });
+        });
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err);
+                res.json(todos);
+            });
+        });
+    });
+};
